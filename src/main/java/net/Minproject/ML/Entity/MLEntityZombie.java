67b719f716f9b7cc1f0f3afec2df6744400e8630
cvronmin <lung1a16@hotmail.com>
2014-08-24 00:06:50 +0800

    Create MLEntityZombie.java

---
+++ src/main/java/net/Minproject/ML/Entity/MLEntityZombie.java
@@ -0,0 +1,370 @@
+package net.Minproject.ML.Entity;
+
+import java.util.Calendar;
+import cpw.mods.fml.client.registry.RenderingRegistry;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
+import cpw.mods.fml.common.registry.EntityRegistry;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.Block;
+import net.minecraft.client.model.ModelZombie;
+import net.minecraft.client.renderer.entity.RenderLiving;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityList;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.IEntityLivingData;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.*;
+import net.minecraft.entity.ai.attributes.*;
+import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.entity.passive.EntityVillager;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.MathHelper;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.EnumDifficulty;
+import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeModContainer;
+
+public class MLEntityZombie extends EntityMob{
+    protected static final IAttribute field_110186_bp = (new RangedAttribute("zombie.spawnReinforcements", 0.0D, 0.0D, 1.0D)).setDescription("Spawn Reinforcements Chance");
+    private final EntityAIBreakDoor field_146075_bs = new EntityAIBreakDoor(this);
+    private boolean field_146076_bu = false;
+    private float field_146074_bv = -1.0F;
+    private float field_146073_bw;
+
+	public MLEntityZombie(World par1World) {
+		super(par1World);
+        this.getNavigator().setBreakDoors(true);
+        this.tasks.addTask(0, new EntityAISwimming(this));
+        this.tasks.addTask(2, new EntityAIAttackOnCollide(this, EntityPlayer.class, 1.0D, false));
+        this.tasks.addTask(4, new EntityAIAttackOnCollide(this, EntityVillager.class, 1.0D, true));
+        this.tasks.addTask(5, new EntityAIMoveTowardsRestriction(this, 1.0D));
+        this.tasks.addTask(6, new EntityAIMoveThroughVillage(this, 1.0D, false));
+        this.tasks.addTask(7, new EntityAIWander(this, 1.0D));
+        this.tasks.addTask(8, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+        this.tasks.addTask(8, new EntityAILookIdle(this));
+        this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true));
+        this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityPlayer.class, 0, true));
+        this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityVillager.class, 0, false));
+        this.setSize(0.6F, 1.8F);
+	}
+    protected void applyEntityAttributes()
+    {
+        super.applyEntityAttributes();
+        this.getEntityAttribute(SharedMonsterAttributes.followRange).setBaseValue(40.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0.23000000417232513D);
+        this.getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(3.0D);
+        this.getAttributeMap().registerAttribute(field_110186_bp).setBaseValue(this.rand.nextDouble() * ForgeModContainer.zombieSummonBaseChance);
+    }
+
+    protected void entityInit()
+    {
+        super.entityInit();
+        this.getDataWatcher().addObject(12, Byte.valueOf((byte)0));
+        this.getDataWatcher().addObject(13, Byte.valueOf((byte)0));
+        this.getDataWatcher().addObject(14, Byte.valueOf((byte)0));
+    }
+    public int getTotalArmorValue()
+    {
+        int i = super.getTotalArmorValue() + 2;
+        if (i > 20)
+        {
+            i = 20;
+        }
+        return i;
+    }
+    protected boolean isAIEnabled()
+    {
+        return true;
+    }
+
+    public boolean func_146072_bX()
+    {
+        return this.field_146076_bu;
+    }
+
+    public void func_146070_a(boolean p_146070_1_)
+    {
+        if (this.field_146076_bu != p_146070_1_)
+        {
+            this.field_146076_bu = p_146070_1_;
+
+            if (p_146070_1_)
+            {
+                this.tasks.addTask(1, this.field_146075_bs);
+            }
+            else
+            {
+                this.tasks.removeTask(this.field_146075_bs);
+            }
+        }
+    }
+    protected int getExperiencePoints(EntityPlayer par1EntityPlayer)
+    {
+        return super.getExperiencePoints(par1EntityPlayer);
+    }
+    public void onLivingUpdate()
+    {
+        if (this.worldObj.isDaytime() && !this.worldObj.isRemote)
+        {
+            float f = this.getBrightness(1.0F);
+
+            if (f > 0.5F && this.rand.nextFloat() * 30.0F < (f - 0.4F) * 2.0F && this.worldObj.canBlockSeeTheSky(MathHelper.floor_double(this.posX), MathHelper.floor_double(this.posY), MathHelper.floor_double(this.posZ)))
+            {
+                boolean flag = true;
+                ItemStack itemstack = this.getEquipmentInSlot(4);
+
+                if (itemstack != null)
+                {
+                    if (itemstack.isItemStackDamageable())
+                    {
+                        itemstack.setItemDamage(itemstack.getItemDamageForDisplay() + this.rand.nextInt(2));
+
+                        if (itemstack.getItemDamageForDisplay() >= itemstack.getMaxDamage())
+                        {
+                            this.renderBrokenItemStack(itemstack);
+                            this.setCurrentItemOrArmor(4, (ItemStack)null);
+                        }
+                    }
+
+                    flag = false;
+                }
+
+                if (flag)
+                {
+                    this.setFire(8);
+                }
+            }
+        }
+
+        super.onLivingUpdate();
+    }
+    public boolean attackEntityFrom(DamageSource par1DamageSource, float par2)
+    {
+        if (!super.attackEntityFrom(par1DamageSource, par2))
+        {
+            return false;
+        }
+        else
+        {
+            EntityLivingBase entitylivingbase = this.getAttackTarget();
+
+            if (entitylivingbase == null && this.getEntityToAttack() instanceof EntityLivingBase)
+            {
+                entitylivingbase = (EntityLivingBase)this.getEntityToAttack();
+            }
+
+            if (entitylivingbase == null && par1DamageSource.getEntity() instanceof EntityLivingBase)
+            {
+                entitylivingbase = (EntityLivingBase)par1DamageSource.getEntity();
+            }
+
+
+            int i = MathHelper.floor_double(this.posX);
+            int j = MathHelper.floor_double(this.posY);
+            int k = MathHelper.floor_double(this.posZ);
+            return true;
+        }
+    }
+    public boolean attackEntityAsMob(Entity par1Entity)
+    {
+        boolean flag = super.attackEntityAsMob(par1Entity);
+
+        if (flag)
+        {
+            int i = this.worldObj.difficultySetting.getDifficultyId();
+
+            if (this.getHeldItem() == null && this.isBurning() && this.rand.nextFloat() < (float)i * 0.3F)
+            {
+                par1Entity.setFire(2 * i);
+            }
+        }
+
+        return flag;
+    }
+    protected String getLivingSound()
+    {
+        return "mob.zombie.say";
+    }
+    protected String getHurtSound()
+    {
+        return "mob.zombie.hurt";
+    }
+    protected String getDeathSound()
+    {
+        return "mob.zombie.death";
+    }
+
+    protected void func_145780_a(int p_145780_1_, int p_145780_2_, int p_145780_3_, Block p_145780_4_)
+    {
+        this.playSound("mob.zombie.step", 0.15F, 1.0F);
+    }
+    protected void addRandomArmor()
+    {
+        super.addRandomArmor();
+
+        if (this.rand.nextFloat() < (this.worldObj.difficultySetting == EnumDifficulty.HARD ? 0.05F : 0.01F))
+        {
+            int i = this.rand.nextInt(3);
+
+            if (i == 0)
+            {
+                this.setCurrentItemOrArmor(0, new ItemStack(Items.iron_sword));
+            }
+            else
+            {
+                this.setCurrentItemOrArmor(0, new ItemStack(Items.iron_shovel));
+            }
+        }
+    }
+    public void writeEntityToNBT(NBTTagCompound par1NBTTagCompound)
+    {
+        super.writeEntityToNBT(par1NBTTagCompound);
+        par1NBTTagCompound.setBoolean("CanBreakDoors", this.func_146072_bX());
+    }
+    public void readEntityFromNBT(NBTTagCompound par1NBTTagCompound)
+    {
+        super.readEntityFromNBT(par1NBTTagCompound);
+        this.func_146070_a(par1NBTTagCompound.getBoolean("CanBreakDoors"));
+    }
+    public void onKillEntity(EntityLivingBase par1EntityLivingBase)
+    {
+        super.onKillEntity(par1EntityLivingBase);
+
+        if ((this.worldObj.difficultySetting == EnumDifficulty.NORMAL || this.worldObj.difficultySetting == EnumDifficulty.HARD) && par1EntityLivingBase instanceof EntityVillager)
+        {
+            if (this.rand.nextBoolean())
+            {
+                return;
+            }
+
+            MLEntityZombie entityzombie = new MLEntityZombie(this.worldObj);
+            entityzombie.copyLocationAndAnglesFrom(par1EntityLivingBase);
+            this.worldObj.removeEntity(par1EntityLivingBase);
+            entityzombie.onSpawnWithEgg((IEntityLivingData)null);
+            this.worldObj.spawnEntityInWorld(entityzombie);
+            this.worldObj.playAuxSFXAtEntity((EntityPlayer)null, 1016, (int)this.posX, (int)this.posY, (int)this.posZ, 0);
+        }
+    }
+    public IEntityLivingData onSpawnWithEgg(IEntityLivingData par1EntityLivingData)
+    {
+        Object p_110161_1_1 = super.onSpawnWithEgg(par1EntityLivingData);
+        float f = this.worldObj.func_147462_b(this.posX, this.posY, this.posZ);
+        this.setCanPickUpLoot(this.rand.nextFloat() < 0.55F * f);
+
+        if (p_110161_1_1 == null)
+        {
+            p_110161_1_1 = new MLEntityZombie.GroupData(this.worldObj.rand.nextFloat() < ForgeModContainer.zombieBabyChance, this.worldObj.rand.nextFloat() < 0.05F, null);
+        }
+
+        if (p_110161_1_1 instanceof MLEntityZombie.GroupData)
+        {
+            MLEntityZombie.GroupData groupdata = (MLEntityZombie.GroupData)p_110161_1_1;
+        }
+        this.func_146070_a(this.rand.nextFloat() < f * 0.1F);
+        this.addRandomArmor();
+        this.enchantEquipment();
+
+        if (this.getEquipmentInSlot(4) == null)
+        {
+            Calendar calendar = this.worldObj.getCurrentDate();
+
+            if (calendar.get(2) + 1 == 10 && calendar.get(5) == 31 && this.rand.nextFloat() < 0.25F)
+            {
+                this.setCurrentItemOrArmor(4, new ItemStack(this.rand.nextFloat() < 0.1F ? Blocks.lit_pumpkin : Blocks.pumpkin));
+                this.equipmentDropChances[4] = 0.0F;
+            }
+        }
+
+        this.getEntityAttribute(SharedMonsterAttributes.knockbackResistance).applyModifier(new AttributeModifier("Random spawn bonus", this.rand.nextDouble() * 0.05000000074505806D, 0));
+        double d0 = this.rand.nextDouble() * 1.5D * (double)this.worldObj.func_147462_b(this.posX, this.posY, this.posZ);
+
+        if (d0 > 1.0D)
+        {
+            this.getEntityAttribute(SharedMonsterAttributes.followRange).applyModifier(new AttributeModifier("Random zombie-spawn bonus", d0, 2));
+        }
+
+        if (this.rand.nextFloat() < f * 0.05F)
+        {
+            this.getEntityAttribute(field_110186_bp).applyModifier(new AttributeModifier("Leader zombie bonus", this.rand.nextDouble() * 0.25D + 0.5D, 0));
+            this.getEntityAttribute(SharedMonsterAttributes.maxHealth).applyModifier(new AttributeModifier("Leader zombie bonus", this.rand.nextDouble() * 3.0D + 1.0D, 2));
+            this.func_146070_a(true);
+        }
+
+        return (IEntityLivingData)p_110161_1_1;
+    }
+    @SideOnly(Side.CLIENT)
+    public void handleHealthUpdate(byte par1)
+    {
+        if (par1 == 16)
+        {
+            this.worldObj.playSound(this.posX + 0.5D, this.posY + 0.5D, this.posZ + 0.5D, "mob.zombie.remedy", 1.0F + this.rand.nextFloat(), this.rand.nextFloat() * 0.7F + 0.3F, false);
+        }
+        else
+        {
+            super.handleHealthUpdate(par1);
+        }
+    }
+    public void func_146071_k(boolean p_146071_1_)
+    {
+        this.func_146069_a(p_146071_1_ ? 0.5F : 1.0F);
+    }
+    protected final void setSize(float par1, float par2)
+    {
+        boolean flag = this.field_146074_bv > 0.0F && this.field_146073_bw > 0.0F;
+        this.field_146074_bv = par1;
+        this.field_146073_bw = par2;
+
+        if (!flag)
+        {
+            this.func_146069_a(1.0F);
+        }
+    }
+    protected final void func_146069_a(float p_146069_1_)
+    {
+        super.setSize(this.field_146074_bv * p_146069_1_, this.field_146073_bw * p_146069_1_);
+    }
+    class GroupData implements IEntityLivingData
+    {
+        public boolean field_142048_a;
+        public boolean field_142046_b;
+
+        private GroupData(boolean par2, boolean par3)
+        {
+            this.field_142048_a = false;
+            this.field_142046_b = false;
+            this.field_142048_a = par2;
+            this.field_142046_b = par3;
+        }
+
+        GroupData(boolean par2, boolean par3, Object par4EntityZombieINNER1)
+        {
+            this(par2, par3);
+        }
+    }
+    
+	public static int mobid = 1010;
+	public Object instance;
+	int entityID = mobid;
+	public void registerRenderers(){
+		RenderingRegistry.registerEntityRenderingHandler(MLEntityZombie.class, new RenderLiving(new ModelZombie(), 0){protected ResourceLocation getEntityTexture(Entity par1Entity){return new ResourceLocation("textures/mlentity/zombie.png");}});
+	}
+	public void serverLoad(FMLServerStartingEvent event){
+		
+	}
+	public void preInit(FMLPreInitializationEvent event){
+		int entityID = EntityRegistry.findGlobalUniqueEntityId();
+		EntityRegistry.registerGlobalEntityID(MLEntityZombie.class, "Zombie(ML)", entityID);
+		EntityRegistry.registerModEntity(MLEntityZombie.class, "Zombie(ML)", entityID, instance, 64, 1, true);
+		EntityList.entityEggs.put(Integer.valueOf(entityID), new EntityList.EntityEggInfo(entityID,  44975, 7969893));        
+	}
+	public void load() {
+	
+	}
+}
