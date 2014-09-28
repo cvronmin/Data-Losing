e9d884e20b28469e4323a2013aacbfebfdcf8dcf
cvronmin <lung1a16@hotmail.com>
2014-09-28 13:44:02 +0800

    Create RItemSpiderEye.java

---
+++ src/main/java/net/Minproject/ML/RItem/RItemSpiderEye.java
@@ -0,0 +1,11 @@
+package net.Minproject.ML.RItem;
+
+import net.minecraft.item.Item;
+
+public class RItemSpiderEye extends Item{
+	public RItemSpiderEye(){
+		setUnlocalizedName("rSpiderEye");
+		setTextureName("rSpider_eye");
+		setCreativeTab(RItemTab.tab);
+	}
+}
