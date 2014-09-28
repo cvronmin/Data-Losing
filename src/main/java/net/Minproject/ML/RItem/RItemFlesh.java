e83bba34f0113cbb0a4042c5cb4f551e75172ce6
cvronmin <lung1a16@hotmail.com>
2014-09-28 13:41:39 +0800

    Create RItemFlesh.java

---
+++ src/main/java/net/Minproject/ML/RItem/RItemFlesh.java
@@ -0,0 +1,11 @@
+package net.Minproject.ML.RItem;
+
+import net.minecraft.item.Item;
+
+public class RItemFlesh extends Item{
+	public RItemFlesh(){
+		setUnlocalizedName("rRottenFlesh");
+		setTextureName("rRotten_flesh");
+		setCreativeTab(RItemTab.tab);
+}
+}
