5abee77126a1292d2154e05ea5bd5bf14b4f5c5b
cvronmin <lung1a16@hotmail.com>
2014-09-28 13:42:34 +0800

    Create RItemString.java

---
+++ src/main/java/net/Minproject/ML/RItem/RItemString.java
@@ -0,0 +1,11 @@
+package net.Minproject.ML.RItem;
+
+import net.minecraft.item.Item;
+
+public class RItemString extends Item{
+	public RItemString(){
+		setUnlocalizedName("rString");
+		setTextureName("rString");
+		setCreativeTab(RItemTab.tab);
+	}
+}
