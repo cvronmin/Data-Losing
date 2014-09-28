cc18366658ee93feecfe8739185742d9368329ba
cvronmin <lung1a16@hotmail.com>
2014-09-28 13:38:47 +0800

    Create RItemGunPowder.java

---
+++ src/main/java/net/Minproject/ML/RItem/RItemGunPowder.java
@@ -0,0 +1,11 @@
+package net.Minproject.ML.RItem;
+
+import net.minecraft.item.Item;
+
+public class RItemGunPowder extends Item{
+public RItemGunPowder(){
+	setUnlocalizedName("rSulphur");
+	setTextureName("rGunpowder");
+	setCreativeTab(RItemTab.tab);
+}
+}
