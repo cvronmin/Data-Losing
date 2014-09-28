b44bc320f955887a63b2a97e6c551c919d3c867b
cvronmin <lung1a16@hotmail.com>
2014-09-28 13:45:02 +0800

    Create RItemSlimeBall.java

---
+++ src/main/java/net/Minproject/ML/RItem/RItemSlimeBall.java
@@ -0,0 +1,11 @@
+package net.Minproject.ML.RItem;
+
+import net.minecraft.item.Item;
+
+public class RItemSlimeBall extends Item{
+	public RItemSlimeBall(){
+		setUnlocalizedName("rSlimeball");
+		setTextureName("rSlimeball");
+		setCreativeTab(RItemTab.tab);
+	}
+}
